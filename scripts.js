7226a34464cab28c02ee9a3aee1116759383d393
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-06 21:02:06 +0200

    spacing

--- scripts.js
+++ scripts.js
@@ -10,30 +10,41 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const newAmount = -parseInt(leoBalance) + -parseFloat(sarahBalance);//parseInt has a tendency of rounding off whereas Parsefloat accomodates decimals
+//parseInt has a tendency of rounding off whereas Parsefloat accomodates decimals
+/*const leoBalanceA =-parseInt(leoBalance);
+const sarahBalanceA =-parseFloat(sarahBalance);
+const leoConvert = (leoBalanceA.toFixed(2));
+const sarahConvert = (sarahBalanceA.toFixed(2));
+const newAmount = parseFloat(leoConvert) + parseFloat(sarahConvert);
+console.log (newAmount)
+
 const owed = (newAmount.toFixed(2));
 
 //console.log('R', parseInt(leoBalance) + parseFloat(sarahBalance));
 const leo = `${leoName} ${leoSurname}(Owed : R${-parseInt(leoBalance).toFixed(2)})`;
+
 const sarah = `${sarahName}${sarahSurname}(Owed : R${-parseFloat(sarahBalance).toFixed(2)})`;
-const total = "Total amount owed:";
+
+const total = "Total amount owed: ";
 const formatAmount = newAmount.toLocaleString ('en-ZA', { style: 'currency', currency: 'ZAR' });
 const result = `${leo}\n${sarah}\n${divider}\n${total}${formatAmount}\n${divider}`;
-console.log(result)
+console.log(result)*/
 
-/*/ Or
-const newAmount = -parseInt(leoBalance) + -parseFloat(sarahBalance);
+ const leoConvert = parseInt (leoBalance * -1).toFixed(2);
+const sarahConvert = parseFloat(sarahBalance * -1).toFixed(2);
+const newAmount = parseFloat(leoConvert) + parseFloat(sarahConvert);
 
 const owed = (newAmount.toFixed(2));
 
-const leo = `${leoName} ${leoSurname}(Owed : R${-parseInt(leoBalance).toFixed(2)})`;
-const sarah = `${sarahName}${sarahSurname}(Owed : R${-parseFloat(sarahBalance).toFixed(2)})`;
-const total = "Total amount owed:";
+const leo = `${leoName} ${leoSurname}(Owed : R${leoConvert})`;
+const sarah = `${sarahName}${sarahSurname}(Owed : R${sarahConvert})`;
+const total = "Total amount owed: ";
 
 const { format } = new Intl.NumberFormat('en-ZA', {
     style: 'currency',
     currency: 'ZAR',
   });
+
 const result = `${leo}\n${sarah}\n${divider}\n${total}${format(owed)}\n${divider}`;
 
-console.log(result)*/
+console.log(result)
