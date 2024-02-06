5ff9ecc5c5091d8288ec0db97999332708995c2f
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-06 13:13:45 +0200

    currency

--- scripts.js
+++ scripts.js
@@ -11,12 +11,29 @@ const divider = '----------------------------------'
 // Only change below this line
 
 const newAmount = -parseInt(leoBalance) + -parseFloat(sarahBalance);//parseInt has a tendency of rounding off whereas Parsefloat accomodates decimals
-const owed =`R ${newAmount.toFixed(2)}`
+const owed = (newAmount.toFixed(2));
 
 //console.log('R', parseInt(leoBalance) + parseFloat(sarahBalance));
 const leo = `${leoName} ${leoSurname}(Owed : R${-parseInt(leoBalance).toFixed(2)})`;
 const sarah = `${sarahName}${sarahSurname}(Owed : R${-parseFloat(sarahBalance).toFixed(2)})`;
 const total = "Total amount owed:";
-const result = `${leo}\n${sarah}\n${divider}\n${total}${owed}\n${divider}`
+const formatAmount = newAmount.toLocaleString ('en-ZA', { style: 'currency', currency: 'ZAR' });
+const result = `${leo}\n${sarah}\n${divider}\n${total}${formatAmount}\n${divider}`;
+console.log(result)
 
-console.log(result);
+/*/ Or
+const newAmount = -parseInt(leoBalance) + -parseFloat(sarahBalance);
+
+const owed = (newAmount.toFixed(2));
+
+const leo = `${leoName} ${leoSurname}(Owed : R${-parseInt(leoBalance).toFixed(2)})`;
+const sarah = `${sarahName}${sarahSurname}(Owed : R${-parseFloat(sarahBalance).toFixed(2)})`;
+const total = "Total amount owed:";
+
+const { format } = new Intl.NumberFormat('en-ZA', {
+    style: 'currency',
+    currency: 'ZAR',
+  });
+const result = `${leo}\n${sarah}\n${divider}\n${total}${format(owed)}\n${divider}`;
+
+console.log(result)*/
